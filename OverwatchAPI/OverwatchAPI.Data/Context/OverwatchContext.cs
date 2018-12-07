c179969137d73f50e0488aca8a212c9cf4b459f0
Serge <sergey.pupinin@student.pxl.be>
2018-12-07 13:03:24 +0100

    made context and added initial migration

---
+++ OverwatchAPI/OverwatchAPI.Data/Context/OverwatchContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using OverwatchAPI.Domain.DomainClasses.Dashboard;
+using OverwatchAPI.Domain.DomainClasses.Projects;
+using OverwatchAPI.Domain.DomainClasses.Widgets;
+
+namespace OverwatchAPI.Data.Context
+{
+    public class OverwatchContext : DbContext
+    {
+        public DbSet<Dashboard> Dashboards { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public Widget Widgets { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=OverwatchDb;Trusted_Connection=True;");
+        }
+    }
+}
